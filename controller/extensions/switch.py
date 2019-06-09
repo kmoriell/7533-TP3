6a136bd26a52199a6f6fcfb1afe7ba3bda70df45
dylan <dylanalvarez1995@gmail.com>
2019-06-09 19:15:39 -0300

    Update fuckery with a working hardcoded example of ping host 1 to 5

--- controller/extensions/switch.py
+++ controller/extensions/switch.py
@@ -18,31 +18,46 @@ class SwitchController:
         y no encuentra en su tabla una regla para rutearlo
         """
         packet = event.parsed
-        if self.dpid == 5:
-            msg = of.ofp_flow_mod()
-            msg.match = of.ofp_match.from_packet(packet, event.port)
-            msg.idle_timeout = 10
-            msg.hard_timeout = 30
-            msg.actions.append(of.ofp_action_output(port=3))
-            msg.data = event.ofp
-            self.connection.send(msg)
 
-        if self.dpid == 2:
-            msg = of.ofp_flow_mod()
-            msg.match = of.ofp_match.from_packet(packet, event.port)
-            msg.idle_timeout = 10
-            msg.hard_timeout = 30
-            msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[2][5]))
-            msg.data = event.ofp
-            self.connection.send(msg)
+        if packet.src.to_str() == '00:00:00:00:00:01':
+            if self.dpid == 5:
+                msg = of.ofp_packet_out()
+                msg.data = event.ofp
+                msg.actions.append(of.ofp_action_output(port=3))
+                self.connection.send(msg)
+            if self.dpid == 2:
+                msg = of.ofp_packet_out()
+                msg.data = event.ofp
+                msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[2][5]))
+                self.connection.send(msg)
+            if self.dpid == 1:
+                msg = of.ofp_packet_out()
+                msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[1][2]))
+                msg.data = event.ofp
+                msg.buffer_id = event.ofp.buffer_id
+                msg.in_port = event.port
+                event.connection.send(msg)
 
-        if self.dpid == 1:
-            msg = of.ofp_flow_mod()
-            msg.match = of.ofp_match.from_packet(packet, event.port)
-            msg.idle_timeout = 10
-            msg.hard_timeout = 30
-            msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[1][2]))
-            msg.data = event.ofp
-            self.connection.send(msg)
+                # ^ DO THINGS LIKE THIS, WITH BUFFER ID AND IN PORT
+                # msg = of.ofp_packet_out()
+                # msg.data = event.ofp
+                # msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[1][2]))
+                # self.connection.send(msg)
+        else:
+            if self.dpid == 5:
+                msg = of.ofp_packet_out()
+                msg.data = event.ofp
+                msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[5][2]))
+                self.connection.send(msg)
+            if self.dpid == 2:
+                msg = of.ofp_packet_out()
+                msg.data = event.ofp
+                msg.actions.append(of.ofp_action_output(port=self.main_controller.ports[2][1]))
+                self.connection.send(msg)
+            if self.dpid == 1:
+                msg = of.ofp_packet_out()
+                msg.data = event.ofp
+                msg.actions.append(of.ofp_action_output(port=1))
+                self.connection.send(msg)
 
-        # log.info("Packet arrived to switch %s from %s to %s", self.dpid, packet.src, packet.dst)
+        log.info("Packet arrived to switch %s from %s to %s", self.dpid, packet.src, packet.dst)
