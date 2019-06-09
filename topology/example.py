f299a8815810295cb9c57e01b951a8f55a6b483a
kmoriell <kmoriell@notebook>
2019-06-09 02:50:35 -0300

    topologia fat-tree

--- topology/example.py
+++ topology/example.py
@@ -10,21 +10,52 @@ from mininet.topo import Topo
 class Example( Topo ):
   def __init__( self, half_ports = 2, **opts ):
     Topo.__init__(self, **opts)
+    
 
-    # Primero creamos los dos switches
-    sw1 = self.addSwitch('sw1')
-    sw2 = self.addSwitch('sw2')
+    niveles = 3
+    cant_de_sw = 0
+    for i in range(1, niveles + 1):
+      cant_de_sw += 2 ** (i - 1)
+    print("Cantidad de switches " + str(cant_de_sw))
+    switches = [None] * cant_de_sw
 
-    # Luego creamos los dos hosts
+    # Primero creo los 3 hosts que se conectan a la raiz    
     h1 = self.addHost('h1')
     h2 = self.addHost('h2')
+    h3 = self.addHost('h3')
 
-    """
-    Por ultimo, creamos los links para terminar con una topologia con la siguiente forma
-    h1 --- sw1 ---- sw2 ---- h2
-    """
-    self.addLink(sw1, sw2) # Conectando los dos switches
-    self.addLink(sw1, h1) # Conectando el sw1 con h1
-    self.addLink(sw2, h2) # Conectando el sw2 con h2
+    sw_count = 1
+    host_count = 4
+    for nivel in range(1, niveles + 1):
+      pos = 2 ** (nivel - 1) - 1      
+      for i in range(0, pos + 1):        
+        switches[pos + i] = self.addSwitch('sw' + str(sw_count))
+        sw_count += 1    
+
+    # Agrego los links
+    sw1 = switches[0]
+    self.addLink(sw1, h1)
+    self.addLink(sw1, h2)
+    self.addLink(sw1, h3)
+
+    for nivel in range(1, niveles + 1):
+      pos = 2 ** (nivel - 1) - 1
+      if nivel == niveles:
+        # Estoy en el ultimo nivel, entonces tengo que agregarle a cada sw de este nivel
+        # un host
+        for i in range(0, pos + 1):
+          sw_actual = switches[pos + i]
+          host_it = self.addHost('h' + str(host_count))
+          host_count += 1
+          self.addLink(sw_actual, host_it) 
+        break
+      for i in range(0, pos + 1):
+        print("actual " + str(pos) + " + " + str(i))
+        sw_actual = switches[pos + i]
+        pos_sig_nivel = 2 ** nivel - 1        
+        for j in range(0, pos_sig_nivel + 1):
+          print("sig " + str(pos_sig_nivel) + " + " + str(j))
+          sw_it = switches[pos_sig_nivel + j]
+          self.addLink(sw_actual, sw_it)
 
 topos = { 'example': Example }
