299ac72baa58c9d07004d146b12b869221baf85c
kmoriell <kmoriell@notebook>
2019-06-12 16:54:07 -0300

    principio de ECMP

---
+++ controller/extensions/_10Tuple.py
@@ -0,0 +1,36 @@
+class _10Tuple:
+    port_in = None
+    eth_src = None
+    eth_dst = None
+    eth_type = None
+    ip_src = None
+    ip_dst = None
+    ip_proto = None
+    tcp_src = None
+    tcp_dst = None
+    def __init__(self, port_in, eth_src, eth_dst, eth_type, ip_src, ip_dst, ip_proto, tcp_src, tcp_dst):
+        self.port_in = None # port_in
+        self.eth_src = eth_src
+        self.eth_dst = eth_dst
+        self.eth_type = eth_type
+        self.ip_src = ip_src
+        self.ip_dst = ip_dst
+        self.ip_proto = ip_proto
+        self.tcp_dst = tcp_dst
+        self.tcp_src = tcp_src
+    def __hash__(self):
+        return hash((self.port_in, self.eth_src, self.eth_dst, self.eth_type, self.ip_src, self.ip_dst, self.ip_proto, self.tcp_dst, self.tcp_src))
+    def __eq__(self, other):
+        if other == None:
+            return False
+        return  (self.port_in == other.port_in and
+            self.eth_src == other.eth_src and
+            self.eth_dst == other.eth_dst and
+            self.eth_type == other.eth_type and
+            self.ip_src == other.ip_src and
+            self.ip_dst == other.ip_dst and
+            self.ip_proto == other.ip_proto and
+            self.tcp_dst == other.tcp_dst and
+            self.tcp_src == other.tcp_src)
+        
+
--- controller/extensions/switch.py
+++ controller/extensions/switch.py
@@ -1,11 +1,15 @@
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 import networkx as nx
+from pox.lib.addresses import IPAddr, EthAddr
+import pox.lib.packet as pkt
+from _10Tuple import _10Tuple
 
 log = core.getLogger()
 
 
 class SwitchController:
+    TCAM = {}
     def __init__(self, dpid, connection, main_controller):
         self.dpid = dpid
         self.connection = connection
@@ -21,12 +25,68 @@ class SwitchController:
         packet = event.parsed
 
         log.info("Packet arrived to switch %s from %s to %s", self.dpid, packet.src, packet.dst)
-        try:
-            path = nx.dijkstra_path(
-                self.main_controller.topology,
-                packet.src.to_str(),
-                packet.dst.to_str()
-            )
+        port = self.dpid #self.main_controller.ports[self.dpid][path[path.index(self.dpid) + 1]]
+
+        _10tupla = None
+        log.info('PKT: ' + pkt.ETHERNET.ethernet.getNameForType(packet.type))
+        if pkt.ETHERNET.ethernet.getNameForType(packet.type) != 'IPV6':                
+        #try:
+            if packet.payload.protocol == packet.payload.TCP_PROTOCOL:
+
+                _10tupla = _10Tuple(port, packet.src, packet.dst, 0x800, packet.payload.srcip, packet.payload.dstip, 0x6, packet.payload.payload.srcport, packet.payload.payload.dstport) 
+            else:
+                _10tupla = _10Tuple(port, packet.src, packet.dst, 0x800, None, None, None, None, None) 
+        else:
+            #if packet.payload.protocol == packet.payload.TCP_PROTOCOL:
+            log.info(pkt.ETHERNET.ethernet.getNameForType(packet.type))
+
+        #except:
+        #    pass
+
+
+        log.info("_10tupla " + str(_10tupla))
+
+        log.info("TCAM keys = " + str(self.TCAM.keys()))
+
+        if _10tupla not in self.TCAM.keys():        
+            log.info("NO esta en TCAM")
+            try:
+                path = nx.dijkstra_path(
+                    self.main_controller.topology,
+                    packet.src.to_str(),
+                    packet.dst.to_str()
+                )
+                log.info("path: " + str(path))            
+
+                # Trafico hacia ultimo elemento de path debe ser enviado por puerto definido en port
+                msg = of.ofp_flow_mod()
+                msg.data = event.ofp
+                msg.idle_timeout = 10
+                msg.hard_timeout = 30
+                msg.match.dl_src = packet.src
+                msg.match.dl_dst = packet.dst            
+
+                if packet.payload.protocol == packet.payload.TCP_PROTOCOL:
+                    msg.match.dl_type = 0x800 # IPv4        
+                    msg.match.nw_proto = 6 # TCP       
+                    msg.match.tp_src = packet.payload.payload.srcport 
+                    msg.match.tp_dst = packet.payload.payload.dstport
+
+                    msg.match.nw_src = packet.payload.srcip 
+                    msg.match.nw_dst = packet.payload.dstip
+                    _10tupla = _10Tuple(port, packet.src, packet.dst, 0x800, packet.payload.srcip, packet.payload.dstip, 0x6, packet.payload.payload.srcport, packet.payload.payload.dstport) 
+                else:
+                    _10tupla = _10Tuple(port, packet.src, packet.dst, 0x800, None, None, None, None, None) 
+                #elif packet.payload.protocol == packet.payload.UDP_PROTOCOL:
+                self.TCAM[_10tupla] = path
+                msg.actions.append(of.ofp_action_output(port = port))
+                event.connection.send(msg)
+                log.info("Installing %s.%i -> %s.%i" %(packet.src, event.ofp.in_port, packet.dst, port))            
+            except nx.NetworkXNoPath:
+                pass
+        else:
+            log.info("Ruta ya en TCAM")
+            path = self.TCAM[_10tupla]
             msg = of.ofp_packet_out()
             msg.actions.append(of.ofp_action_output(
                 port=self.main_controller.ports[self.dpid][path[path.index(self.dpid) + 1]]
@@ -34,5 +94,3 @@ class SwitchController:
             msg.data = event.ofp
             event.connection.send(msg)
 
-        except nx.NetworkXNoPath:
-            pass
